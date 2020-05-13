2fc44e22be1cc2ec9b6dac288b72ec7bbb5da809
utkarsh-coding <62289436+utkarsh-coding@users.noreply.github.com>
2020-05-13 21:56:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,138 @@
+var database;
+
+var drawing = [];
+var currentPath = [];
+var isDrawing = false;
+
+function setup() {
+  canvas = createCanvas(200, 200);
+
+  canvas.mousePressed(startPath);
+  canvas.parent('canvascontainer');
+  canvas.mouseReleased(endPath);
+
+  var saveButton = select('#saveButton');
+  saveButton.mousePressed(saveDrawing);
+
+  var clearButton = select('#clearButton');
+  clearButton.mousePressed(clearDrawing);
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyBc2T1I0ca_OOwIcrma24BZSxh_WGs7obQ",
+    authDomain: "drawing-c4aca.firebaseapp.com",
+    databaseURL: "https://drawing-c4aca.firebaseio.com",
+    projectId: "drawing-c4aca",
+    storageBucket: "drawing-c4aca.appspot.com",
+    messagingSenderId: "897049856097",
+    appId: "1:897049856097:web:787d8c22fe8b7287970220"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var params = getURLParams();
+  console.log(params);
+  if (params.id) {
+    console.log(params.id);
+    showDrawing(params.id);
+  }
+
+
+}
+
+function startPath() {
+  isDrawing = true;
+  currentPath = [];
+  drawing.push(currentPath);
+}
+
+function endPath() {
+  isDrawing = false;
+}
+
+function draw() {
+  background(0);
+
+  if (isDrawing) {
+    var point = {
+      x: mouseX,
+      y: mouseY
+    };
+    currentPath.push(point);
+  }
+
+  stroke(255);
+  strokeWeight(4);
+  noFill();
+  for (var i = 0; i < drawing.length; i++) {
+    var path = drawing[i];
+    beginShape();
+    for (var j = 0; j < path.length; j++) {
+      vertex(path[j].x, path[j].y);
+    }
+    endShape();
+  }
+}
+
+function saveDrawing() {
+  var ref = database.ref('drawings');
+  var data = {
+    name: 'Joy',
+    drawing: drawing
+  };
+  var result = ref.push(data, dataSent);
+  console.log(result.key);
+
+  function dataSent(err, status) {
+    console.log(status);
+  }
+}
+
+function gotData(data) {
+  // clear the listing
+  var elts = selectAll('.listing');
+  for (var i = 0; i < elts.length; i++) {
+    elts[i].remove();
+  }
+
+  var drawings = data.val();
+  var keys = Object.keys(drawings);
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    //console.log(key);
+    var li = createElement('li', '');
+    li.class('listing');
+    var ahref = createA('#', key);
+    ahref.mousePressed(showDrawing);
+    ahref.parent(li);
+
+    var perma = createA('?id=' + key, 'permalink');
+    perma.parent(li);
+    perma.style('padding', '4px');
+
+    li.parent('drawinglist');
+  }
+}
+
+function errData(err) {
+  console.log(err);
+}
+
+function showDrawing(key) {
+  //console.log(arguments);
+  if (key instanceof MouseEvent) {
+    key = this.html();
+  }
+
+  var ref = database.ref('drawings/' + key);
+  ref.once('value', oneDrawing, errData);
+
+  function oneDrawing(data) {
+    var dbdrawing = data.val();
+    drawing = dbdrawing.drawing;
+    //console.log(drawing);
+  }
+}
+
+function clearDrawing() {
+  drawing = [];
+}
